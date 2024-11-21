bb50e7a976c90c0c3a552b52b9d917850d5fa30d
Gabriel Matos <matos.gabriel2013@gmail.com>
2024-11-21 10:20:11 -0300

    fix: add the AddControllers service

--- Reservation.API/Program.cs
+++ Reservation.API/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Reservation.Application.Interfaces.Services;
 using Reservation.Data.Context;
+using Reservation.Data.Repository;
+using Reservation.Domain.Interfaces.Repository;
 using Reservation.Identity.Data;
+using Reservation.Identity.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,11 +21,14 @@ builder.Services.AddDbContext<IdentityDataContext>(options => {
     options.UseSqlServer(connectionString);
 });
 
-builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<IdentityDataContext>()
     .AddDefaultTokenProviders();
 
-builder.Services.AddDataProtection();
+builder.Services.AddScoped<ITableRepository, TableRepository>();
+builder.Services.AddScoped<IIdentityService, IdentityService>();
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
