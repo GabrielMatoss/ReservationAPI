32dd8ef2814c0d92b609640a7a8d0f7bea3669b6
Gabriel Matos <matos.gabriel2013@gmail.com>
2024-11-20 22:39:17 -0300

    feat: creating interface for IdentityService

---
+++ Reservation.Application/Interfaces/Services/IIdentityService.cs
@@ -0,0 +1,10 @@
+using Reservation.Application.DTOs.Request;
+using Reservation.Application.DTOs.Response;
+
+namespace Reservation.Application.Interfaces.Services;
+
+public interface IIdentityService
+{
+    Task<UserRegisterResponse> RegisterUser(UserRegisterRequest userRegisterRequest);
+    Task<UserLoginResponse> Login(UserLoginRequest userLoginResponse);
+}
