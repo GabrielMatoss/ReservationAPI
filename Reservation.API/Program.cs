edfedb179f4adac52f0b26eef6e720c18e8fc3a3
Gabriel Matos <dev.matosgabriel@gmail.com>
2024-12-13 22:36:24 -0300

    feat: add configs to authentication and swagger

--- Reservation.API/Program.cs
+++ Reservation.API/Program.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Reservation.API.Configurations;
 using Reservation.Application.Interfaces.Services;
+using Reservation.Application.Services;
 using Reservation.Data.Context;
 using Reservation.Data.Repository;
 using Reservation.Domain.Interfaces.Repository;
+using Reservation.Domain.Interfaces.Services;
 using Reservation.Identity.Data;
 using Reservation.Identity.Services;
 
@@ -25,13 +30,49 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<IdentityDataContext>()
     .AddDefaultTokenProviders();
 
-builder.Services.AddScoped<ITableRepository, TableRepository>();
+builder.Services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+
 builder.Services.AddScoped<IIdentityService, IdentityService>();
+builder.Services.AddScoped<ITableService, TableService>();
+builder.Services.AddScoped<ITableRepository, TableRepository>();
 
 builder.Services.AddControllers();
+builder.Services.AddConfigAuthentication(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. 
+                      Enter 'Bearer' [space] and then your token in the text input below. 
+                      Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+           Reference = new OpenApiReference
+           {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+           },
+                Scheme = "Bearer",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+        },
+            new List<string>()
+        }
+    });
+});
+
+builder.Logging.AddConsole();
 
 var app = builder.Build();
 
@@ -43,6 +84,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
