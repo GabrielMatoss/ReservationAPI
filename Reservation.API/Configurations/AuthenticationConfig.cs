c292b8e7fa381fef82227ffb16ed83a26862fe62
Gabriel Matos <dev.matosgabriel@gmail.com>
2024-12-13 22:32:45 -0300

    feat: add authentication configuration

---
+++ Reservation.API/Configurations/AuthenticationConfig.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Reservation.Identity.Configurations;
+using System.Text;
+
+namespace Reservation.API.Configurations;
+
+public static class AuthenticationConfig
+{
+    public static void AddConfigAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtAppSettingOptions = configuration.GetSection(nameof(JwtOptions));
+        var secret = configuration.GetSection("JwtOptions:SecurityKey").Value;
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret!));
+
+        services.Configure<JwtOptions>(options =>
+        {
+            options.Issuer = jwtAppSettingOptions[nameof(JwtOptions.Issuer)]!;
+            options.Audience = jwtAppSettingOptions[nameof(JwtOptions.Audience)]!;
+            options.SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
+            options.AccessTokenExpiration = int.Parse(jwtAppSettingOptions[nameof(JwtOptions.AccessTokenExpiration)] ?? "0");
+        });
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = true;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequiredLength = 6;
+        });
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = configuration.GetSection("JwtOptions:Issuer").Value,
+                ValidateAudience = true,
+                ValidAudience = configuration.GetSection("JwtOptions:Audience").Value,
+
+                ValidateIssuerSigningKey = true,
+                ValidateLifetime = true,
+                IssuerSigningKey = securityKey,
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+    }
+}
