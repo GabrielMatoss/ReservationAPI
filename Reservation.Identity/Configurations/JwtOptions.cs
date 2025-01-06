ec310b106954ed90f75928e7a90d4130290b6a01
Gabriel Matos <dev.matosgabriel@gmail.com>
2025-01-05 23:12:32 -0300

    fix: initialize JwtOptions properties to prevent null reference exceptions

--- Reservation.Identity/Configurations/JwtOptions.cs
+++ Reservation.Identity/Configurations/JwtOptions.cs
@@ -4,8 +4,8 @@ namespace Reservation.Identity.Configurations;
 
 public class JwtOptions
 {
-    public SigningCredentials SigningCredentials { get; set; }
+    public SigningCredentials SigningCredentials { get; set; } = null!;
     public int AccessTokenExpiration { get; set; }
-    public string Issuer { get; set; }
-    public string Audience { get; set; }
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
 }
