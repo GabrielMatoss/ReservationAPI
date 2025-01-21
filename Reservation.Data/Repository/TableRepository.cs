1317c724a25da68aab634dd98b215a031b28ef0f
Gabriel Matos <dev.matosgabriel@gmail.com>
2025-01-21 14:44:07 -0300

    refactor: remove commented duplicate methods in TableRepository

--- Reservation.Data/Repository/TableRepository.cs
+++ Reservation.Data/Repository/TableRepository.cs
@@ -9,4 +9,18 @@ public class TableRepository : RepositoryBase<Table>, ITableRepository
 {
     public TableRepository(DataContext dataContext) : base(dataContext)
     {}
+    /*
+    //funciona pq meu GetByIdAsync � um numero
+    public async Task<Table?> GetTableByNumberAsync(int tableNumber)
+    {
+        return await base.GetByIdAsync(tableNumber);
+    }
+    */
+    /*
+    //Eu não preciso disso pelo fato de que o GetByIdAsync já faz isso
+    public async Task<Table?> GetTableByNumberAsync(int tableNumber)
+    {
+        return await base.GetByIdAsync(tableNumber);
+    }
+    */
 }
