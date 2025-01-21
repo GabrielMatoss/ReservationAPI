20861d97ed5cf21276029c3e30da065821b99183
Gabriel Matos <dev.matosgabriel@gmail.com>
2025-01-21 11:22:47 -0300

    refactor: unify date and time properties in ReserveResponse for improved clarity

--- Reservation.Application/DTOs/Response/ReserveResponse.cs
+++ Reservation.Application/DTOs/Response/ReserveResponse.cs
@@ -5,24 +5,18 @@ namespace Reservation.Application.DTOs.Response;
 
 public class ReserveResponse
 {
-    public DateTime DateReserve { get; set; }
-    public TimeSpan TimeReserve { get; set; }
-    public int TableNumber { get; set; }
-    public IEnumerable<GuestResponse> Guests { get; set; }
-    public ReserveResponse(DateTime dateReserve, TimeSpan timeReserve, int tableNumber, IEnumerable<GuestResponse> guests)
+    public DateTime DateTimeReserve { get; set; }
+    public int TableId { get; set; }
+    public ReserveResponse(DateTime dateTimeReserve, int tableId)
     {
-        DateReserve = dateReserve;
-        TimeReserve = timeReserve;
-        TableNumber = tableNumber;
-        Guests = guests ?? [];
+        DateTimeReserve = dateTimeReserve;
+        TableId = tableId;
     }
     public static ReserveResponse ConvertToResponse(Reserve reserve)
     {
         return new ReserveResponse(
-            reserve.DateReserve,
-            reserve.TimeReserve,
-            reserve.TableNumber,
-            reserve.Guests.Select(g =>
-            new GuestResponse(g?.Name, g?.LastName)));
+            reserve.DateTimeReserve,
+            reserve.TableId
+        );
     }
 }
