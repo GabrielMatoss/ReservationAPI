a4f25d94f07f50ec20fca7db9e8a09f37e72533c
Gabriel Matos <dev.matosgabriel@gmail.com>
2025-01-21 11:25:01 -0300

    refactor: adjustment in the formatting of IIdentityService

--- Reservation.Application/Interfaces/Services/IIdentityService.cs
+++ Reservation.Application/Interfaces/Services/IIdentityService.cs
@@ -3,7 +3,7 @@ using Reservation.Application.DTOs.Response;
 
 namespace Reservation.Application.Interfaces.Services;
 
-public interface IIdentityService
+public interface IIdentityService 
 {
     Task<UserRegisterResponse> RegisterUser(UserRegisterRequest userRegisterRequest);
     Task<UserLoginResponse> Login(UserLoginRequest userLoginResponse);
