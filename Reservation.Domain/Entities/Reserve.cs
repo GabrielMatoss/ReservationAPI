404039c5086710a762a3e0bdb87d15515d9af82b
Gabriel Matos <dev.matosgabriel@gmail.com>
2025-01-20 23:32:16 -0300

    refactor: simplify Reserve entity by removing TableNumber and Guests properties

--- Reservation.Domain/Entities/Reserve.cs
+++ Reservation.Domain/Entities/Reserve.cs
@@ -3,17 +3,13 @@ namespace Reservation.Domain.Entities;
 public class Reserve : EntityBase
 {
     public DateTime DateTimeReserve { get; set; } = DateTime.Now;
-    public int TableNumber { get; set; }
     public int TableId { get; set; }
     public string UserId { get; set; } = null!;
-    public List<Guest> Guests { get; set; } = [];
-
     public Reserve() { }
-    public Reserve(DateTime dateTimeReserve, int tableNumber, List<Guest> guests, string userId)
+    public Reserve(DateTime dateReserve, int tableId, string userId)
     {
-        TableNumber = tableNumber;
-        DateTimeReserve = dateTimeReserve;
-        Guests = guests;
+        DateTimeReserve = dateReserve;
+        TableId = tableId;
         UserId = userId;
     }
 }
