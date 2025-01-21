f788536f2d5576b17ac8016823daacb21aa66fa3
Gabriel Matos <dev.matosgabriel@gmail.com>
2025-01-21 14:41:38 -0300

    fix: update table initialization data with new values

--- Reservation.Data/Mappings/TableMappings.cs
+++ Reservation.Data/Mappings/TableMappings.cs
@@ -13,11 +13,10 @@ public class TableMappings : IEntityTypeConfiguration<Table>
         builder.Property(e => e.Id).ValueGeneratedOnAdd();
 
         builder.HasData(
-            new Table(1, 3, 2),
-            new Table(2, 5, 4),
-            new Table(3, 4, 2),
-            new Table(4, 2, 2),
-            new Table(5, 1, 4)
-            );
+            new Table(1, 1, 2),
+            new Table(2, 2, 4),
+            new Table(3, 3, 2),
+            new Table(4, 4, 2),
+            new Table(5, 5, 4));
     }
 }
