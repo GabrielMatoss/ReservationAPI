33146a3d0b488f8397a6666af0f22f830a36b1d2
Gabriel Matos <dev.matosgabriel@gmail.com>
2025-01-21 14:34:42 -0300

    refactor: enhance TableController with documentation improvements and method simplification

--- Reservation.API/Controllers/TableController.cs
+++ Reservation.API/Controllers/TableController.cs
@@ -17,7 +17,6 @@ public class TableController : ControllerBase
         _tableService = tableService;
     }
     
-    [Authorize]
     [ProducesResponseType(typeof(IEnumerable<TableResponse>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpGet]
@@ -32,6 +31,8 @@ public class TableController : ControllerBase
         return Ok(tablesResponse);
     }
 
+    [ProducesResponseType(typeof(TableResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [Authorize]
     [HttpGet("{id}")]
     public async Task<ActionResult<TableResponse>> SelectById(int id)
@@ -44,6 +45,8 @@ public class TableController : ControllerBase
         return Ok(tableResponse);
     }
 
+    [ProducesResponseType(typeof(TableResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [Authorize(Roles = Roles.Admin)]
     [HttpPost]
     public async Task<ActionResult<TableResponse>> AddNewTable(TableRequest tableRequest)
@@ -58,9 +61,11 @@ public class TableController : ControllerBase
         return CreatedAtAction(nameof(SelectById), new { id }, id);
     }
 
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [Authorize(Roles = Roles.Admin)]
     [HttpDelete("{id}")]
-    public async Task<ActionResult> DeleteTableById(int id)
+    public async Task<IActionResult> DeleteTableById(int id)
     {
         var table = await _tableService.GetByIdAsync(id);
         if (table == null)
@@ -71,23 +76,26 @@ public class TableController : ControllerBase
         return Ok("Table deleted!");
     }
 
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [Authorize(Roles = Roles.Admin)]
     [HttpPut]
-    public async Task<ActionResult> UpdateTable(int id, TableRequest tableRequest)
+    public async Task<IActionResult> UpdateTable(int id, TableRequest tableRequest)
     {
         var trackedTable = await _tableService.GetByIdAsync(id);
 
         if (trackedTable == null)
-            NotFound("Table not founded");
-        
-        var table = TableRequest.ConvertToEntity(tableRequest);
-        
-        trackedTable!.TableNumber = table.TableNumber;
-        trackedTable.Reserves = table.Reserves;
-        trackedTable.Capacity = table.Capacity;
+            return NotFound("Table not found");
+
+        trackedTable.TableNumber = tableRequest.TableNumber;
+        trackedTable.Capacity = tableRequest.Capacity;
+
+        if (trackedTable.Reserves != null)
+            trackedTable.Reserves = TableRequest.ConvertToEntity(tableRequest).Reserves;
 
         await _tableService.UpdateAsync(id, trackedTable);
 
         return Ok("Table updated!");
     }
+    
 }
